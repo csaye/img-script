371441160ef95532aa18bbb63496412050da5510
Cooper Saye <coopersaye@gmail.com>
2021-05-05 20:10:54 -0600

    Implement character printing.

--- Interpreter.py
+++ Interpreter.py
@@ -9,4 +9,13 @@ pixels = program.load()
 for y in range(height):
     for x in range(width):
 
-        # process pixel
+        # get pixel and pixel data
+        pixel = pixels[x, y]
+        r, g, b, a = pixel
+
+        # skip if alpha 0
+        if a == 0: continue
+
+        # print character
+        if r == 0:
+            print(chr(g + b), end='')
