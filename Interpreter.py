c86f4ae651ffa296d932e4bed1a06e8d67290b33
Cooper Saye <coopersaye@gmail.com>
2021-05-12 20:49:44 -0600

    Improve if statements.

--- Interpreter.py
+++ Interpreter.py
@@ -18,9 +18,6 @@ class Var:
     def __init__(self, value, reading):
         self.value = value
         self.reading = reading
-        
-    value = ''
-    reading = False
 
 # open program
 try:
@@ -54,7 +51,7 @@ def value(pixel):
     r, g, b, a = pixel
 
     # return if empty or not value pixel
-    if a == 0 or r != 0: return ''
+    if a == 0 or r != 0: return None
 
     # return value
     if g == 0: return chr(b) # ascii
@@ -76,7 +73,7 @@ def process(i):
     if r == 0:
 
         # get value as string
-        val = str(value(pixel))
+        val = value(pixel)
 
         # for each var in varlist
         building = False
@@ -86,7 +83,8 @@ def process(i):
             if varlist[i].reading:
 
                 # append value
-                varlist[i].value += val
+                if varlist[i].value == None: varlist[i].value = val
+                else: varlist[i].value += val
                 building = True
 
         # if not building
@@ -99,16 +97,47 @@ def process(i):
     # variable definition
     elif r == 1:
 
-        if g == 0: varlist[b] = Var('', True) # start read
-        if g == 1: varlist[b].reading = False # end read
+        # read
+        if g == 0: varlist[b] = Var(None, True) # start read
+        elif g == 1: varlist[b].reading = False # end read
+
+        # modify
+        elif g == 2: varlist[b].value += 1 # add 1
+        elif g == 3: varlist[b].value -= 1 # sub 1
 
     # if statement
     elif r == 2:
 
-        term1 = value(pixels[index + 1]) # term 1
-        operation = value(pixels[index + 2]) # operation
-        term2 = value(pixels[index + 3]) # term 2
-        result = value(pixels[index + 4]) # result
+        # two term statement
+        if g >= 0 and g < 4:
+
+            # get statement values
+            term1 = value(pixels[index + 1]) # term 1
+            term2 = value(pixels[index + 2]) # term 2
+
+            index += 2 # skip if statement pixels
+
+            if g == 0 and term1 != term2: index += b # equals
+            elif g == 1 and term1 == term2: index += b # not equals
+            elif g == 2 and term1 <= term2: index += b # greater than
+            elif g == 3 and term1 >= term3: index += b # less than
+
+        # three term statement
+        if g >= 4 and g < 8:
+
+            # get statement values
+            term1 = value(pixels[index + 1]) # term 1
+            operation = value(pixels[index + 2]) # operation
+            term2 = value(pixels[index + 3]) # term 2
+            term3 = value(pixels[index + 4]) # term 3
+
+            index += 4 # skip if statement pixels
+
+            # if statement does not pass, skip pixels
+            if g == 4 and operation(term1, term2) != term3: index += b # equals
+            elif g == 5 and operation(term1, term2) == term3: index += b # not equals
+            elif g == 6 and operation(term1, term2) <= term3: index += b # greater than
+            elif g == 7 and operation(term1, term2) >= term3: index += b # less than
 
     # goto statement
     elif r == 3:
